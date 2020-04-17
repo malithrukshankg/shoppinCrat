653f37a39e34ce41d244b1eaed74770167cbc3b0
mkoswattegedara <mcrukshan75@gmail.com>
2020-04-17 20:41:33 +0530

    Adding components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Drirectory from '../../components/directory/directory.component'
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Drirectory></Drirectory>
+  </div>
+);
+
+export default HomePage;
