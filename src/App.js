a1772b587bd39a7b97d6748afc9832be6e5abe93
mkoswattegedara <mcrukshan75@gmail.com>
2020-04-25 20:02:50 +0530

    Adding redux

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
-import {Switch,Route} from 'react-router-dom'
+import {Switch,Route,Redirect} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUp from'./pages/sign-in-and-sign-up-page/sign-in-and-sign-up.component'
 import {auth,createUserProfileDocument} from './firebase/firebase.utils'
-
+import {connect} from 'react-redux'
+import { setCurrentUser} from './redux/user/user.actions'
 
 class App extends React.Component {
 
-  constructor(){
-    super();
-    this.state={
-      currentUser:null
-    }
-  }
+  
 
   unsubscribeFromAuth:null;
 
   componentDidMount() {
+     
+    const { setCurrentUser }=this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+        setCurrentUser({
+            
               id: snapShot.id,
               ...snapShot.data()
-            }
+           
           });
 
           console.log(this.state);
         });
       }
 
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
   }
 
@@ -47,11 +45,11 @@ class App extends React.Component {
   render(){
     return (
       <div >
-      <Header currentUser={this.state.currentUser}></Header>
+      <Header></Header>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
-        <Route exact path='/signin' component={SignInAndSignUp} />
+        <Route exact path='/signin' render={()=>this.props.currentUser?(<Redirect to='/'/>):(<SignInAndSignUp/>)}/>
       </Switch>
        </div>
     );
@@ -59,4 +57,16 @@ class App extends React.Component {
  
 }
 
-export default App;
+const mapSateToProps=state=>({
+
+  currentUser: state.user.currentUser
+
+})
+
+const mapDipatchToProps=dispatch=>({
+
+setCurrentUser:user=>dispatch(setCurrentUser(user))
+})
+
+export default connect(mapSateToProps
+  ,mapDipatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
 import {Route} from 'react-router-dom'
+import store from './redux/store';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
     
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
